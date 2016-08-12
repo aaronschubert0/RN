f314f9ed142408e5c6202f11ce5fd0967f96bf04
Aaron Schubert <aaronschubert0@googlemail.com>
2016-08-12 09:43:17 +0100

    Parse out FullscreenArticle

--- index.ios.js
+++ index.ios.js
@@ -3,6 +3,7 @@ import { AppRegistry, View, Text, Image, ListView, NavigationExperimental, Touch
 import { TabNavigator, Tab } from './js/TabNavigator/'
 import { Meta, Divider, InfoPanel } from './js/Components/'
 import { FeaturedImage, SmallImage, Opinion, Live, Sponsored } from './js/FeedItems/'
+import FullscreenArticle from './js/Article/FullscreenArticle'
 const {
   CardStack: NavigationCardStack,
   StateUtils: NavigationStateUtils,
@@ -84,44 +85,6 @@ class Metro extends Component {
     }
     return <Component {...props} {...navigationActions} />
   }
-}
-
-const FullScreenArticle = ({ articleData, sectionTitle, pop }) => {
-  const { title, imageURL } = articleData
-  return (
-    <View style={{ backgroundColor: 'white', flex: 1 }}>
-      <View style={{ backgroundColor: 'white', borderBottomColor: 'gray', borderBottomWidth: 0.5, height: 64, flexDirection:'row', justifyContent: 'space-between', alignItems: 'center', paddingTop: 20, paddingLeft: 20, paddingRight: 20 }}>
-        <TouchableOpacity onPress={() => pop()}>
-          <Text style={{ color: '#54565b', fontFamily:'Source Sans Pro', fontSize: 16 }}>{'< Back'}</Text>
-        </TouchableOpacity>
-        <Text style={{ color: '#09b4ff', fontFamily:'Source Sans Pro', fontSize: 16 }}>{sectionTitle}</Text>
-        <Text style={{ color: '#54565b', fontFamily:'Source Sans Pro', fontSize: 16 }}>Share</Text>
-      </View>
-      <ScrollView style={{ flex: 1}}>
-        <Image
-        source={{ uri: imageURL }}
-        style={{ width: 375, height: 211 }}
-        />
-        <View style={{ flex: 1, backgroundColor: 'white', paddingLeft: 20, paddingRight: 20 }}>
-          <Text style={{fontSize: 22, fontWeight: '700', fontFamily: 'Source Sans Pro', paddingBottom: 15, paddingTop: 15}}>
-          {title}
-          </Text>
-          <Text style={{ color: '#54565b', fontSize: 16, paddingBottom: 15, lineHeight: 25 }}>
-            The First Minister announced a new Scotland-specific Child Poverty Bill to improve life chances.
-          </Text>
-          <Text style={{ color: '#54565b', fontSize: 16, paddingBottom: 15, lineHeight: 25 }}>
-            Legislation aimed at helping to eradicate child poverty in Scotland has been announced by Nicola Sturgeon.
-          </Text>
-          <Text style={{ color: '#54565b', fontSize: 16, paddingBottom: 15, lineHeight: 25 }}>
-            The First Minister said the Child Poverty Bill will set out a new approach to tackling inequality as she visited the Prince's Trust charity.
-          </Text>
-          <Text style={{ color: '#54565b', fontSize: 16, paddingBottom: 15, lineHeight: 25 }}>
-            She was also sharply critical of the UK Government's decision last year to repeal much of the Child Poverty Act, calling it "fundamentally wrong".
-          </Text>
-        </View>
-      </ScrollView>
-    </View>
-  )
 }
 
 const ArticleSwitcher = ({ fullscreen, article, children }) => {
@@ -247,7 +210,7 @@ class One extends Component {
     return (
       object.type ?
       <TouchableOpacity
-        onPress={() => this.props.push(FullScreenArticle,
+        onPress={() => this.props.push(FullscreenArticle,
          { articleData: object, sectionTitle: this.props.title },
           'article')}>
         <Article article={object} />
---
+++ js/Article/FullscreenArticle.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import  { View, TouchableOpacity, Image, ScrollView, Text } from 'react-native'
+
+const FullscreenArticle = ({ articleData, sectionTitle, pop }) => {
+  console.log('Render ')
+  const { title, imageURL } = articleData
+  return (
+    <View style={{ backgroundColor: 'white', flex: 1 }}>
+      <View style={{ backgroundColor: 'white', borderBottomColor: 'gray', borderBottomWidth: 0.5, height: 64, flexDirection:'row', justifyContent: 'space-between', alignItems: 'center', paddingTop: 20, paddingLeft: 20, paddingRight: 20 }}>
+        <TouchableOpacity onPress={() => pop()}>
+          <Text style={{ color: '#54565b', fontFamily:'Source Sans Pro', fontSize: 16 }}>{'< Back'}</Text>
+        </TouchableOpacity>
+        <Text style={{ color: '#09b4ff', fontFamily:'Source Sans Pro', fontSize: 16 }}>{sectionTitle}</Text>
+        <Text style={{ color: '#54565b', fontFamily:'Source Sans Pro', fontSize: 16 }}>Share</Text>
+      </View>
+      <ScrollView style={{ flex: 1}}>
+        <Image
+        source={{ uri: imageURL }}
+        style={{ width: 375, height: 211 }}
+        />
+        <View style={{ flex: 1, backgroundColor: 'white', paddingLeft: 20, paddingRight: 20 }}>
+          <Text style={{fontSize: 22, fontWeight: '700', fontFamily: 'Source Sans Pro', paddingBottom: 15, paddingTop: 15}}>
+          {title}
+          </Text>
+          <Text style={{ color: '#54565b', fontSize: 16, paddingBottom: 15, lineHeight: 25 }}>
+            The First Minister announced a new Scotland-specific Child Poverty Bill to improve life chances.
+          </Text>
+          <Text style={{ color: '#54565b', fontSize: 16, paddingBottom: 15, lineHeight: 25 }}>
+            Legislation aimed at helping to eradicate child poverty in Scotland has been announced by Nicola Sturgeon.
+          </Text>
+          <Text style={{ color: '#54565b', fontSize: 16, paddingBottom: 15, lineHeight: 25 }}>
+            The First Minister said the Child Poverty Bill will set out a new approach to tackling inequality as she visited the Prince's Trust charity.
+          </Text>
+          <Text style={{ color: '#54565b', fontSize: 16, paddingBottom: 15, lineHeight: 25 }}>
+            She was also sharply critical of the UK Government's decision last year to repeal much of the Child Poverty Act, calling it "fundamentally wrong".
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
+
+export default FullscreenArticle
