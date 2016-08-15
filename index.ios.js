9a9008f6181c7f90e39cac6db55b9a69c9fa3076
Aaron Schubert <aaronschubert0@googlemail.com>
2016-08-15 16:42:45 +0100

    Render tombstones until network request comes back

--- index.ios.js
+++ index.ios.js
@@ -84,9 +84,5 @@ class Metro extends Component {
     return <Component {...props} {...navigationActions} />
   }
 }
-import requestRankedArticlesInSection from './js/API/requestRankedArticlesInSection'
-
-    requestRankedArticlesInSection(3057).then(console.log)
-
 
 AppRegistry.registerComponent('Metro', () => Metro)
