bd3b9af243d1da6a1fd416ccf851c4052388e2b4
Soutar <john@soutar.me>
2016-08-16 15:07:29 +0100

    Use 60 seconds as default cache maxAge

--- js/API/request.js
+++ js/API/request.js
@@ -1,14 +1,16 @@
 import { getBaseUrl } from './environments'
 import { readCache, cacheResponse } from './cache'
 
+const defaultMaxAge = 60
+
 export default function request (pathQuery) {
   const baseUrl = getBaseUrl()
   const url = `${baseUrl}/${pathQuery}`
   const cached = readCache(url)
-  console.log('URL', url)
   return cached ? Promise.resolve(cached) : fetch(url).then(
       response => {
-          const [ , maxAge] = response.headers.get('cache-control').match(/max-age=([^,]+)/)
+          const cacheControl = response.headers.get('cache-control') || ''
+          const maxAge = cacheControl.match(/max-age=([^,]+)/)[1] || defaultMaxAge
           const expiry = Date.now() + parseInt(maxAge*1000)
           return response.json().then(response => {
             if (!response.success) {
